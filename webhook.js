3963f54b8b38e4f644e4affac2c6d7e7e51b53ab
ideaspaper <agni.infinity@gmail.com>
2020-06-16 19:53:41 +0700

    added message-time

--- webhook.js
+++ webhook.js
@@ -3,11 +3,12 @@ const getQuote = require('./helper/quote-helper').getQuote;
 const sendGif = require('./helper/send-helper').sendGif;
 const sendMessage = require('./helper/send-helper').sendMessage;
 const sendQuote = require('./helper/send-helper').sendQuote;
+const messageTime = require('./message-time.json');
 
 function main() {
   (function loop() {
     var now = new Date();
-    if (now.getHours() === 8 && now.getMinutes() === 30) {
+    if (now.getHours() === messageTime.morning.hour && now.getMinutes() === messageTime.morning.minute) {
       console.log('Morning message');
       getMessage('morning')
         .then(({ message }) => {
@@ -17,19 +18,19 @@ function main() {
         .then(({ quote, author }) => {
           sendQuote(quote, author);
         });
-    } else if (now.getHours() === 12 && now.getMinutes() === 0) {
+    } else if (now.getHours() === messageTime.lunch.hour && now.getMinutes() === messageTime.lunch.minute) {
       console.log('Lunch message');
       getMessage('lunch')
         .then(({ message }) => {
           sendGif('lunch', message);
         });
-    } else if (now.getHours() === 17 && now.getMinutes() === 0) {
+    } else if (now.getHours() === messageTime.standup.hour && now.getMinutes() === messageTime.standup.minute) {
       console.log('Stand-up message');
       getMessage('standup')
         .then(({ message }) => {
           sendMessage(message);
         });
-    } else if (now.getHours() === 18 && now.getMinutes() === 0) {
+    } else if (now.getHours() === messageTime.night.hour && now.getMinutes() === messageTime.night.minute) {
       console.log('Night message');
       getMessage('night')
         .then(({ message }) => {
